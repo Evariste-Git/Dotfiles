ba915b9a9fc56557b964def7f7e07441ffd9383c
Simon Evariste Schirra <simon.evariste@web.de>
2025-07-31 23:23:28 +0200

    style: formatting

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,15 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuchin",
-	priority = 1000,
-	config = function()
-		require("catppuccin").setup({
-			flavour = "macchiato",
+    "catppuccin/nvim",
+    name = "catppuchin",
+    priority = 1000,
+    config = function()
+        require("catppuccin").setup({
+            flavour = "macchiato",
             -- enable automatic integration of other neovim plugins
             auto_integrations = true,
-		})
+        })
 
-		-- apply catppuccin
-		vim.cmd.colorscheme("catppuccin")
-	end,
+        -- apply catppuccin
+        vim.cmd.colorscheme("catppuccin")
+    end,
 }
