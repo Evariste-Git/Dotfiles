632d0388de81221219b2a145a0db36d8e65016e5
Simon Evariste Schirra <simon.evariste@web.de>
2025-07-31 23:13:10 +0200

    fix: use .setup()

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,8 +1,13 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuchin",
-    priority = 1000,
-    config = function()
-        vim.cmd.colorscheme("catppuccin-macchiato")
-    end,
+	"catppuccin/nvim",
+	name = "catppuchin",
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato",
+		})
+
+		-- apply catppuccin
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
