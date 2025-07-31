170db12c4853f627227db2641a997fcf2e21b025
Simon Evariste Schirra <simon.evariste@web.de>
2025-07-31 23:51:06 +0200

    style: readability
    Improve readability by inserting empty lines

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -11,6 +11,7 @@ return {
     config = function()
         require("catppuccin").setup({
             flavour = "macchiato",
+
             -- enable automatic integration of other neovim plugins
             auto_integrations = true,
         })
