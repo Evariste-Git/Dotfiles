1271257216232c1ec22c5e91b69717c925730d1f
Simon Evariste Schirra <simon.evariste@web.de>
2025-07-31 19:52:43 +0200

    style: formatting

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,19 +1,22 @@
 -- regulary run :TSUpdate to update all the parsers
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	-- nvim-treesitter does not support lazy loading
-	lazy = false,
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			-- parsers that are ensured to be installed
-			-- a list of names or "all"
-			ensure_installed = { "c", "css", "just", "lua", "rust" },
-			highlight = { enable = true, additional_vim_regex_highlighting = false },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    branch = "master",
+    -- nvim-treesitter does not support lazy loading
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            -- parsers that are ensured to be installed
+            -- a list of names or "all"
+            ensure_installed = { "c", "css", "just", "lua", "rust" },
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+        })
+    end,
 }
