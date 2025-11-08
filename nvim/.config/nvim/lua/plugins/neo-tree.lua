0315ef1149a6545eabfc095aefa4cae6d0d940e1
Simon Evariste Schirra <simon.evariste@web.de>
2025-11-08 09:39:58 +0100

    feat: show hidden files by default

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -14,6 +14,20 @@ return {
         -- fill any relevant options here
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = false,
+                    hide_by_name = {
+                        '.git',
+                    }
+                }
+            },
+        never_show = {},
+        })
         vim.keymap.set('n', '<C-n>', ':Neotree <CR>')
     end
 }
