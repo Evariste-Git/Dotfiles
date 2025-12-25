92f9f45c5976feac0e639d9d73025fa6146cdf29
Simon Evariste Schirra <simon.evariste@web.de>
2025-12-25 12:27:32 +0100

    feat: make scripts executable and call each other

--- waybar/.config/waybar/launch.sh
+++ waybar/.config/waybar/launch.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # this script only works if waybar is managed by uwsm
 
+# get correct colors
+echo "copying colors"
+"$HOME/Dotfiles/waybar/.config/waybar/copy_theme.sh"
+
+echo "restarting"
 systemctl --user restart waybar.service
