20d0f2c6c7a0140b415a5376d0a0dedf834e1f51
Simon Evariste Schirra <simon.evariste@web.de>
2026-05-02 10:52:11 +0200

    nvim: improve diagnostics

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -15,5 +15,32 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
-vim.diagnostic.config({ virtual_text = true })
+-- diagnostics
+vim.diagnostic.config({
+	virtual_text = false,
+	signs = true,
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
 
+	float = {
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
+})
+vim.o.updatetime = 250
+local hover_enabled = true
+vim.keymap.set("n", "<leader>th", function()
+	hover_enabled = not hover_enabled
+	print("Hover diagnostics: " .. (hover_enabled and "ON" or "OFF"))
+end)
+vim.api.nvim_create_autocmd("CursorHold", {
+	callback = function()
+        if not hover_enabled then return end
+		vim.diagnostic.open_float(nil, {
+			focus = false,
+		})
+	end,
+})
